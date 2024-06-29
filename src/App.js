d32de9478c5525303c92c789250f1387f4ebe03f
Henry <henryng161@gmail.com>
2024-06-29 17:16:48 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+const api = {
+  key: "51f1a56d56a5b28d57a7a2b08fd74cc1",
+  base: "https://openweathermap.org/data/2.5/weather",
+};
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessgae, setErrorMessage] = useState(false);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (!searchCity) return;
+      setLoading(true);
+      // Process
+      try {
+        const url = `${api.base}?q=${searchCity}&units=metric&APPID=${api.key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setWeatherInfo(JSON.stringify(data));
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+
+      setLoading(false);
+    };
+    fetchWeatherData();
+  }, [searchCity]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit} action="">
+        <input
+          onChange={(e) => setSearchInput(e.target.value)}
+          type="text"
+          placeholder="city"
+          value={searchInput}
+        />
+        <button>Search</button>
+      </form>
+      <div>{weatherInfo}</div>
+    </>
   );
 }
 
